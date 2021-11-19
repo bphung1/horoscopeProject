79534820bcb28361492122be64a381cc572dddfa
Tyler Martin <tamart21@asu.edu>
2021-11-19 12:48:42 -0700

    Begun User/Prediction classes and DAOs

---
+++ HoroscopeAPI/Horoscope/src/main/java/mthree/avatar/horoscope/dto/Prediction.java
@@ -0,0 +1,17 @@
+package mthree.avatar.horoscope.dto;
+
+import java.time.LocalDateTime;
+
+public class Prediction {
+
+    private int predictionId;
+    private String username;
+    private LocalDateTime timestamp;
+    private String horoscope;
+    private String fortuneCookie;
+    private int luckyNumbers;
+    private String luckySportsTeam;
+    private String luckyUniversity;
+    private String jokeOfTheDay;
+
+}
