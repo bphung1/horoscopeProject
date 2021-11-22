449acf9d31d099b0855196bfdbf6425f2b923dca
Bao Phung <b.giaphung@gmail.com>
2021-11-22 10:33:33 -0600

    adding content for login - Angular

--- HoroscopeAngular/HoroscopeApp/src/app/login/login.module.ts
+++ HoroscopeAngular/HoroscopeApp/src/app/login/login.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginpageComponent } from './loginpage/loginpage.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [LoginpageComponent],
   imports: [
     CommonModule,
+    FormsModule
   ]
 })
 export class LoginModule { }
--- HoroscopeAngular/HoroscopeApp/src/app/login/loginpage/loginpage.component.ts
+++ HoroscopeAngular/HoroscopeApp/src/app/login/loginpage/loginpage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +8,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./loginpage.component.css']
 })
 export class LoginpageComponent implements OnInit {
+  submitted = false;
+
+  @ViewChild('f', { static: false }) signupForm: NgForm;
 
   constructor(private router: Router) { }
 
