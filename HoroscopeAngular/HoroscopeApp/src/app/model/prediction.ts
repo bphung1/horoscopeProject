6eb920e35d59a464f19ca32ad18bfa22d1e2c1cf
Bao Phung <b.giaphung@gmail.com>
2021-11-22 11:57:24 -0600

    horoscope page content and model (as well as temp data) added - Angular

---
+++ HoroscopeAngular/HoroscopeApp/src/app/model/prediction.ts
@@ -0,0 +1,9 @@
+export interface Prediction {
+    predictionID: number;
+    timestamp: Date;
+    sign: string;
+    horoscope: string;
+    fortuneCookie: string;
+    luckyNumber: number;
+    jokeOfTheDay: string;
+}
---
+++ HoroscopeAngular/HoroscopeApp/src/app/model/predictionTempData.ts
@@ -0,0 +1,86 @@
+
+import { Prediction } from "./prediction";
+
+export const PREDICTIONS: Prediction[] = [
+    {
+        predictionID: 1,
+        sign: 'aquarius',
+        timestamp: new Date('2021-11-22'),
+        horoscope: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        fortuneCookie: 'believe in the heart of the cards',
+        luckyNumber: 7,
+        jokeOfTheDay: "Why can't java see properly? Because Java cannot C#"
+    },
+    {
+        predictionID: 2,
+        sign: 'scorpio',
+        timestamp: new Date('2021-05-11'),
+        horoscope: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit cursus risus at ultrices mi. Tincidunt id aliquet risus feugiat in ante metus. Odio tempor orci dapibus ultrices in iaculis. Cras adipiscing enim eu turpis egestas. Tellus cras adipiscing enim eu. Ullamcorper a lacus vestibulum sed arcu. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Sodales ut etiam sit amet nisl purus in mollis.',
+        fortuneCookie: 'Justo donec enim diam vulputate. Consectetur a erat nam at.',
+        luckyNumber: 3,
+        jokeOfTheDay: "Quam viverra orci sagittis eu volutpat odio facilisis mauris sit."
+    },
+    {
+        predictionID: 3,
+        sign: 'leo',
+        timestamp: new Date('1999-12-25'),
+        horoscope: 'Tincidunt praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Laoreet id donec ultrices tincidunt arcu. Non diam phasellus vestibulum lorem sed risus ultricies. Purus non enim praesent elementum facilisis leo vel fringilla est. Porttitor lacus luctus accumsan tortor posuere ac ut consequat semper.',
+        fortuneCookie: 'Netus et malesuada fames ac turpis.',
+        luckyNumber: 23,
+        jokeOfTheDay: "Lectus arcu bibendum at varius vel pharetra."
+    },
+    {
+        predictionID: 4,
+        sign: 'cancer',
+        timestamp: new Date('2013-01-01'),
+        horoscope: 'At risus viverra adipiscing at in. Ultricies leo integer malesuada nunc vel risus commodo viverra. Blandit massa enim nec dui nunc. Pretium fusce id velit ut tortor pretium viverra. Ipsum dolor sit amet consectetur adipiscing elit.',
+        fortuneCookie: 'At erat pellentesque adipiscing commodo elit at imperdiet dui.',
+        luckyNumber: 8,
+        jokeOfTheDay: "Morbi tristique senectus et netus et malesuada fames ac turpis."
+    },
+    {
+        predictionID: 5,
+        sign: 'capricorn',
+        timestamp: new Date('1993-03-03'),
+        horoscope: 'Quis risus sed vulputate odio ut enim blandit volutpat. Eget aliquet nibh praesent tristique magna sit amet. Praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla. Egestas purus viverra accumsan in. Eget nullam non nisi est sit amet facilisis magna. ',
+        fortuneCookie: 'Congue quisque egestas diam in.',
+        luckyNumber: 30,
+        jokeOfTheDay: "Pretium viverra suspendisse potenti nullam ac."
+    },
+    {
+        predictionID: 6,
+        sign: 'saggitarius',
+        timestamp: new Date('2007-11-22'),
+        horoscope: 'Aliquet eget sit amet tellus cras. Suspendisse in est ante in nibh. Phasellus vestibulum lorem sed risus ultricies tristique. Neque volutpat ac tincidunt vitae semper quis. Aliquam ut porttitor leo a diam sollicitudin tempor id.',
+        fortuneCookie: 'Sed euismod nisi porta lorem mollis. Blandit volutpat maecenas volutpat blandit aliquam etiam.',
+        luckyNumber: 51,
+        jokeOfTheDay: "Ut eu sem integer vitae justo."
+    },
+    {
+        predictionID: 7,
+        sign: 'gemini',
+        timestamp: new Date('2017-02-23'),
+        horoscope: 'Et odio pellentesque diam volutpat. Risus pretium quam vulputate dignissim. Nisl vel pretium lectus quam id leo in vitae. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Pulvinar etiam non quam lacus. Massa eget egestas purus viverra accumsan in nisl.',
+        fortuneCookie: 'Vivamus at augue eget arcu dictum varius duis at consectetur.',
+        luckyNumber: 1,
+        jokeOfTheDay: "Mauris commodo quis imperdiet massa tincidunt nunc pulvinar."
+    },
+    {
+        predictionID: 8,
+        sign: 'virgo',
+        timestamp: new Date('1986-05-05'),
+        horoscope: 'Varius vel pharetra vel turpis nunc eget lorem dolor. Auctor elit sed vulputate mi sit amet mauris commodo. Tortor consequat id porta nibh venenatis cras. Ultrices gravida dictum fusce ut placerat orci nulla pellentesque.',
+        fortuneCookie: 'Aenean vel elit scelerisque mauris. Eu non diam phasellus vestibulum.',
+        luckyNumber: 3,
+        jokeOfTheDay: "Nunc scelerisque viverra mauris in."
+    },
+    {
+        predictionID: 9,
+        sign: 'pisces',
+        timestamp: new Date('1995-08-08'),
+        horoscope: 'Lorem donec massa sapien faucibus. Euismod lacinia at quis risus sed vulputate odio ut. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. In ornare quam viverra orci sagittis eu volutpat odio. Eu turpis egestas pretium aenean pharetra.',
+        fortuneCookie: 'Ultricies integer quis auctor elit sed vulputate mi.',
+        luckyNumber: 70,
+        jokeOfTheDay: "Eu lobortis elementum nibh tellus molestie nunc non."
+    }
+]
