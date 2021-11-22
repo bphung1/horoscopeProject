4e3000dabfca84e8afc1b750d6b0c4f84bcd196e
Jon Pryzby <jpryzby@gmail.com>
2021-11-22 15:12:32 -0500

    added in profile page
    reads dummy data from a prediction.ts file

--- HoroscopeAngular/HoroscopeApp/src/app/profile/profile.module.ts
+++ HoroscopeAngular/HoroscopeApp/src/app/profile/profile.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfilepageComponent } from './profilepage/profilepage.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ProfilepageComponent],
   imports: [
     CommonModule,
+    FormsModule
   ]
 })
 export class ProfileModule { }
